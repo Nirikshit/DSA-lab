6bf0b4c0047d8b3d398f5714e455825febcf5a61
Nirikshit <73182545+Nirikshit@users.noreply.github.com>
2025-11-14 15:26:10 +0530

    Add files via upload

---
+++ Poly_Multiply.c
@@ -0,0 +1,122 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int co,po;
+    struct node *addr;
+};
+typedef struct node *NODE;
+
+NODE insertend(NODE start,int co,int po)
+{
+    NODE temp,cur;
+    temp=(NODE)malloc(sizeof(struct node));
+    temp->co=co;
+    temp->po=po;
+    temp->addr=NULL;
+    if (start==NULL)
+    {
+        return temp;
+    }
+    cur=start;
+    while(cur->addr!=NULL)
+    {
+        cur=cur->addr;
+    }
+    cur->addr=temp;
+    return start;
+}
+
+NODE addterm(NODE result,int co,int po)
+{
+    NODE temp,cur;
+    temp=(NODE)malloc(sizeof(struct node));
+    temp->co=co;
+    temp->po=po;
+    temp->addr=NULL;
+    if (result==NULL)
+    {
+        return temp;
+    }
+    cur=result;
+    while(cur!=NULL)
+    {
+        if(cur->po==po)
+        {
+            cur->co=cur->co+co;
+            return result;
+        }
+        cur=cur->addr;
+    }
+    if(cur==NULL)
+    {
+        result=insertend(result,co,po);
+    }
+    return result;
+}
+
+NODE multiply(NODE poly1,NODE poly2)
+{
+    NODE p1,p2,res=NULL;
+    for(p1=poly1;p1!=NULL;p1=p1->addr)
+    {
+        for(p2=poly2;p2!=NULL;p2=p2->addr)
+        {
+            res=addterm(res,p1->co*p2->co,p1->po+p2->po);
+        }
+    }
+    return res;
+}
+
+void display(NODE start)
+{
+    NODE temp;
+    if(start==NULL)
+    {
+        printf("\nPolynomial is empty");
+    }
+    else
+    {
+        temp=start;
+        while(temp->addr!=NULL)
+        {
+            printf("%d*x^%d+",temp->co,temp->po);
+            temp=temp->addr;
+        }
+        printf("%d*x^%d",temp->co,temp->po);
+    }
+}
+
+
+
+int main()
+{
+    NODE poly1=NULL,poly2=NULL,poly;
+    int n,co,po;
+    printf("\nRead the number of terms of 1st polynomial: ");
+    scanf("%d",&n);
+    for (int i=1;i<=n;i++)
+    {
+        printf("\nRead co and po of term %d : ",i);
+        scanf("%d%d",&co,&po);
+        poly1=insertend(poly1,co,po);
+    }
+    printf("\nFirst Polynomial is \n");
+    display(poly1);
+
+    printf("\n\nRead the number of terms of 2nd polynomial: ");
+    scanf("%d",&n);
+    for (int i=1;i<=n;i++)
+    {
+        printf("\nRead co and po of term %d : ",i);
+        scanf("%d%d",&co,&po);
+        poly2=insertend(poly2,co,po);
+    }
+    printf("\nSecond Polynomial is \n");
+    display(poly2);
+
+    poly=multiply(poly1,poly2);
+    printf("\n\nResultant Polynomial is :\n");
+    display(poly);
+}
